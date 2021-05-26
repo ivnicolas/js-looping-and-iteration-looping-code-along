6c3aa098e9f3a37db7ec562e0c3054b467a6df96
ivnicolas <ikennanicolas@gmail.com>
2021-05-26 15:12:55 -0400

    commit tree eb4625f0d5b5f8fdddae41f6bdbb5b658f9ad5aa

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, eventName){ 
+    let messages = []
+    for (let i= 0; i < array.length; i++){
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages
+}
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
